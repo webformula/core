67ce472dda21b5f2a71f17ce27015b1d8f8948f6
Ben Rubin <ben@benjaminrubin.com>
2023-09-09 17:16:43 -0500

    remove old code

--- docs/routes/index/index.js
+++ docs/routes/index/index.js
@@ -4,15 +4,6 @@ import html from './page.html';
 export default class extends Component {
   static title = 'Home';
   static html = html;
-
-  test = 'test';
-  two = 'two';
-  three = {
-    value: 'three',
-    sub: {
-      value: 'sub'
-    }
-  };
   
   constructor() {
     super();
